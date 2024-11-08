175c2d536c0458c7758588baff8975c746f16929
BojanaVv <bvrzovska@gmail.com>
2024-11-08 11:46:27 +0100

    fetched and mutations functions added

--- pages/components/playerLayout.tsx
+++ pages/components/playerLayout.tsx
@@ -1,45 +1,7 @@
 import { Box } from "@chakra-ui/layout";
 import Sidebar from "./sidebar";
-// import { useEffect } from "react";
 
 const PlayerLayout = ({ children }) => {
-  // Sign up
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     fetch("http://localhost:3000/api/signup", {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       method: "POST",
-  //       body: JSON.stringify({
-  //         email: "test@gmail.com",
-  //         password: "abcabc",
-  //         username: "username1",
-  //       }),
-  //     });
-  //   }
-
-  //   fetchData();
-  // }, []);
-
-  // Sign in
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     fetch("http://localhost:3000/api/signin", {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       method: "POST",
-  //       body: JSON.stringify({
-  //         email: "test@gmail.com",
-  //         password: "abcabc",
-  //       }),
-  //     });
-  //   }
-
-  //   fetchData();
-  // }, []);
-
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
