8018641988f1c1f094de3b693c45e23fa4a3b4bb
BojanaVv <bvrzovska@gmail.com>
2024-11-11 14:32:30 +0100

    middleware token problem fixed

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,17 @@ const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    const token: string = req.cookies.TRAX_ACCESS_TOKEN;
+    const [...cookies] = req.cookies;
 
-    if (!token) {
+    let token: string;
+
+    if ([...cookies].length > 0) {
+      token = cookies[0][1].value;
+    } else {
+      token = "";
+    }
+
+    if (token === "") {
       const url = req.nextUrl.clone();
       url.pathname = "/signin";
       return NextResponse.rewrite(url);
