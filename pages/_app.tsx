f238f17bfcb448bdb4a170c1c901e05a784cdcd2
BojanaVv <bvrzovska@gmail.com>
2024-11-14 11:54:50 +0100

    Small css mistakes solved

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,7 +41,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </PlayerLayout>
       )}
-      ;
     </ChakraProvider>
   );
 }
