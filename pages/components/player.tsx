2fb855caae0adbfb6c5d07706c43b8f075ac16d9
BojanaVv <bvrzovska@gmail.com>
2024-11-14 12:38:56 +0100

    player component added

---
+++ pages/components/player.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import ReactHowler from "react-howler";
+import { useEffect, useRef, useState } from "react";
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from "react-icons/md";
+import { useStoreActions } from "easy-peasy";
+
+const Player = () => {
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center>
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            color="gray.600"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            color="gray.600"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            color="white"
+            aria-label="play"
+            fontSize="40px"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            color="white"
+            aria-label="pause"
+            fontSize="40px"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            color="gray.600"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            color="gray.600"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+    </Box>
+  );
+};
+
+export default Player;
