e651fb4c90883002f826168e5d6c6e6fe1908891
BojanaVv <bvrzovska@gmail.com>
2024-11-18 15:18:12 +0100

    Small changes

--- pages/api/artist.ts
+++ pages/api/artist.ts
@@ -4,12 +4,8 @@ import connectDB from "../../config/database";
 
 // GET api/artist
 export default validateRoute(async (req, res) => {
-  try {
-    await connectDB();
-    const artists = await Artist.find({});
+  await connectDB();
+  const artists = await Artist.find({});
 
-    res.json(artists);
-  } catch (error) {
-    res.json({ status: 404 }, { message: "Something went wrong" });
-  }
+  res.json(artists);
 });
--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -4,11 +4,8 @@ import connectDB from "../../config/database";
 
 // GET api/playlist
 export default validateRoute(async (req, res, user) => {
-  try {
-    await connectDB();
-    const playlists = await Playlist.find({ user: user.id }).sort({ name: 1 });
-    res.json(playlists);
-  } catch (error) {
-    res.json({ status: 404 }, { message: "Something went wrong" });
-  }
+  await connectDB();
+  const playlists = await Playlist.find({ user: user.id }).sort({ name: 1 });
+
+  res.json(playlists);
 });
