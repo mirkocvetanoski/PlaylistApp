8903de58ec3aab5f3b24b40028e6689334c3a231
BojanaVv <bvrzovska@gmail.com>
2024-11-18 15:10:12 +0100

    try catch added

--- pages/api/artist.ts
+++ pages/api/artist.ts
@@ -4,8 +4,12 @@ import connectDB from "../../config/database";
 
 // GET api/artist
 export default validateRoute(async (req, res) => {
-  await connectDB();
-  const artists = await Artist.find({});
+  try {
+    await connectDB();
+    const artists = await Artist.find({});
 
-  res.json(artists);
+    res.json(artists);
+  } catch (error) {
+    res.json({ status: 404 }, { message: "Something went wrong" });
+  }
 });
--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -4,8 +4,11 @@ import connectDB from "../../config/database";
 
 // GET api/playlist
 export default validateRoute(async (req, res, user) => {
-  await connectDB();
-  const playlists = await Playlist.find({ user: user.id }).sort({ name: 1 });
-
-  res.json(playlists);
+  try {
+    await connectDB();
+    const playlists = await Playlist.find({ user: user.id }).sort({ name: 1 });
+    res.json(playlists);
+  } catch (error) {
+    res.json({ status: 404 }, { message: "Something went wrong" });
+  }
 });
