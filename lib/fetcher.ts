d6b4635e1e6e071a691a6cfb1855989535fc1ae9
BojanaVv <bvrzovska@gmail.com>
2024-11-19 14:41:14 +0100

    Refactoring code

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,5 @@
 export default function fetcher(url: string, data = undefined) {
-  return fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api${url}`, {
+  return fetch(`${window.location.origin}/api${url}`, {
     method: data ? "POST" : "GET",
     credentials: "include",
     headers: {
